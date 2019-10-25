d85f907946aa7eb83ecd5bb5d1dcd886926ff0b9
Cheikh Tidiane Diedhiou <cheikht2011@gmail.com>
2019-10-25 10:27:52 +0200

    Ajout de la fonction d'initialisation

--- src/helper.c
+++ src/helper.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+#include <time.h>
 #include "../include/helper.h"
 
 
+int * initialize(int size, int nbBombs) {
+	int * grid = malloc(size * size * sizeof(int));
+	
+	/* Initialisation de toutes les cases à une valeur neutre (-10) */
+	for (int i = 0; i < size*size; i++)
+		grid[i] = -10;
+
+	/* Pose des mines à des positions aléatoires ( Une mine a la valeur -11 )*/ 
+	srand((unsigned)time(NULL)); 
+	for (int i = 0; i < nbBombs; i++)
+		grid[rand() % (size*size)] = -11;
+	
+	return grid;
+}
 
 void putNumbers(int *grid, int size){
 	int neighbours[] = {-size - 1, -size, -size + 1,  -1, 1,  size - 1,  size, size + 1};
