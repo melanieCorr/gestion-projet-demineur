30afdb59e15be273c7f0d8249b18ea191c70ec24
YasmineBOU <yasmineboudjemai10@gmail.com>
2019-10-25 10:45:02 +0200

    Ajout d'un point virgule

--- src/helper.c
+++ src/helper.c
@@ -5,7 +5,7 @@
 
 void putNumbers(int *grid, int size){
 	int neighbours[] = {-size - 1, -size, -size + 1,  -1, 1,  size - 1,  size, size + 1};
-	int nbBombs = 0
+	int nbBombs = 0;
 	for(int i = 0; i < size; ++i, nbBombs = 0){
 		if(grid[i] != BOMB){
 			for(int neigh = 0; neigh < NB_NEIGHBOURS; ++neigh){
